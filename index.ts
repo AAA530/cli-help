e6a78a337c58dae69abd1b420404f6a2fc10ace3
AAA530 <dhairyapatelad@gmail.com>
2024-04-01 19:24:17 -0400

    added ask question function

--- index.ts
+++ index.ts
@@ -12,54 +12,33 @@ config();
 
 const program = new Command();
 
+const configuration ={}
+
 program
   .name('cli-help')
   .description('A command line tool for helping developers')
   .version('0.0.1');
 
 
-const first = (str, options) => {
-  const limit = options.first ? 1 : undefined;
-  console.log(str);
-}
+const first = async (str, options) => {
+  // const limit = options.first ? 1 : undefined;
+  console.log(str)
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: str.text }],
+    model: "gpt-3.5-turbo",
+  });
 
-const second = (str, options) => {
-  const limit = options.first ? 1 : undefined;
-  console.log(str);
+  console.log(completion.choices[0].message.content);
 }
 
 program
-  .option('-f, --first <string>', 'display just the first substring')
+  .option('-q, --question <string>', 'give a prompt to get the answer')
   .action(first);
-
-program
-  .option('-s, --second <string>', 'display just the first substring')
-  .action(second);
 
 
 
 async function main() {
-  // program
-  //   .command('run')
-  //   .action(run);
   await program.parseAsync(process.argv);
 }
 
 main()
-  
-// program.parse();
-
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     messages: [{ role: "system", content: "give mw command to delete a file" }],
-//     model: "gpt-3.5-turbo",
-//   });
-
-//   console.log(completion.choices[0]);
-// }
-
-// console.log(
-//   main().then((res) => {
-//     console.log(res);
-//   })
-// );
